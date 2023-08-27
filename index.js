e9560f1522f01fa3eb71a468e5ba604d41f46cad
Mauricio Cardona <maucaralar@gmail.com>
2023-08-27 18:48:32 -0400

    corrected audio files path

--- index.js
+++ index.js
@@ -27,37 +27,37 @@ function makeSound(key) {
 
     switch (key) {
         case "w":
-            var kick = new Audio("/sounds/KICK.wav");
+            var kick = new Audio("sounds/KICK.wav");
             kick.play();
             break;
 
         case "e":
-            var snare = new Audio("/sounds/SNARE.wav");
+            var snare = new Audio("sounds/SNARE.wav");
             snare.play();
             break;
 
         case "r":
-            var hihat = new Audio("/sounds/HIHAT.wav");
+            var hihat = new Audio("sounds/HIHAT.wav");
             hihat.play();
             break;
 
         case "y":
-            var tom1 = new Audio("/sounds/TOM1.wav");
+            var tom1 = new Audio("sounds/TOM1.wav");
             tom1.play();
             break;
 
         case "u":
-            var tom2 = new Audio("/sounds/TOM2.wav");
+            var tom2 = new Audio("sounds/TOM2.wav");
             tom2.play();
             break;
 
         case "i":
-            var tom3 = new Audio("/sounds/TOM3.wav");
+            var tom3 = new Audio("sounds/TOM3.wav");
             tom3.play();
             break;
 
         case "o":
-            var crash = new Audio("/sounds/CRASH.wav");
+            var crash = new Audio("sounds/CRASH.wav");
             crash.play();
             break;
 
